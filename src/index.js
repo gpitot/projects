e6868cc5c14202a2162ea540b3f735533c693659
Guillaume Pitot <gpitot@nine.com.au>
2019-08-23 09:58:47 +1000

    changed path, and imports

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import style from "./sass/index.scss";
 import js from "./js/scripts.js"; 
 
 
-import vid from "./assets/jiffy-c.mp4";
+import vid1 from "./assets/fifty-festival-c.mp4";
+import vid2 from "./assets/fracture-space-c.mp4";
+import vid3 from "./assets/good-times-c.mp4";
+import vid4 from "./assets/hex-yellow-c.mp4";
+import vid5 from "./assets/jiffy-c.mp4";
+import vid6 from "./assets/lolas-c.mp4";
+import vid7 from "./assets/racquettes-c.mp4";
+import vid8 from "./assets/radio-internet-c.mp4";
+import vid9 from "./assets/succulent-garden-c.mp4";
+import vid10 from "./assets/super-shapes-c.mp4";
+import vid11 from "./assets/valencia-c.mp4";
